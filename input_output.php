10e269c8362abe61ce231576ba439ee99ab7b67c
yogiparmana <49135804+yogiparmana@users.noreply.github.com>
2021-02-28 02:38:30 +0800

    Create input_output.php

---
+++ input_output.php
@@ -0,0 +1,22 @@
+<?php 
+
+echo("Input Console\n");
+echo("-----------------\n");
+    
+    //input menggunakan readline();
+    //$number; //inisialisasi variable $number tidak perlu
+    echo("Masukkan Number : "); //keterangan input
+    $number = readline();// proses input
+    echo("output number : " . $number . "\n"); // proses output
+    
+    
+    //$kata; // inisialisasi variable $kata tidak perlu
+    echo("Masukkan Kata : "); //keterangan input
+    $kata = readline();// proses input
+    echo("output string : " .$kata); // output
+    
+    //ini sangat mudah karena kita tidak perlu menentukan type datanya
+
+
+   
+ ?>
